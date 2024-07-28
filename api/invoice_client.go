7b39f06a0b6b5de9ede488f669c5d9bd50a08834
nt2311-vn <nguyentoan231196@gmail.com>
2024-07-28 13:05:59 +0700

    implement invoice client to connect to invoice api

--- api/invoice_client.go
+++ api/invoice_client.go
@@ -1,1 +1,67 @@
 package api
+
+import (
+	"encoding/json"
+
+	"github.com/go-resty/resty/v2"
+)
+
+const baseURL = "https://vinvoice.viettel.vn/api"
+
+type AuthClient struct {
+	client *resty.Client
+	token  string
+}
+
+type InvoiceClient struct {
+	client     *resty.Client
+	authClient *AuthClient
+}
+
+func NewAuthClient() *AuthClient {
+	client := resty.New()
+
+	return &AuthClient{client: client}
+}
+
+func (c *AuthClient) GetToken(authObj map[string]string) (string, error) {
+	resp, err := c.client.R().
+		SetHeader("Accept", "application/json").
+		SetHeader("Accept-Language", "en-US,en;q=0.9,vi;q=0.8").
+		SetHeader("Content-Type", "application/json;charset=UTF-8").
+		SetHeader("Host", "vinvoice.viettel.vn").
+		SetHeader("Referer", "https://vinvoice.viettel.vn/account/login").
+		SetBody(authObj).Post("https://vinvoice.viettel.vn/api/auth/login")
+	if err != nil {
+		return "", err
+	}
+
+	if resp.IsError() {
+		return "", resp.Error().(error)
+	}
+
+	var result map[string]interface{}
+	err = json.Unmarshal(resp.Body(), &result)
+	if err != nil {
+		return "", err
+	}
+
+	token, ok := result["token"].(string)
+	if !ok {
+		return "", err
+	}
+
+	return token, nil
+}
+
+func NewApiClient(baseURL string, authObj map[string]string) (*InvoiceClient, error) {
+	authClient := NewAuthClient()
+	token, err := authClient.GetToken(authObj)
+	if err != nil {
+		return nil, err
+	}
+
+	client := resty.New().SetBaseURL(baseURL).SetAuthToken(token)
+
+	return &InvoiceClient{client: client, authClient: authClient}, nil
+}
