8e6d4f73d08e08d2290d08bbb45757968c42daa2
nt2311-vn <nguyentoan231196@gmail.com>
2024-07-31 17:17:39 +0700

    update invoice client

--- api/invoice_client.go
+++ api/invoice_client.go
@@ -8,9 +8,10 @@ import (
 
 	"github.com/go-resty/resty/v2"
 	"github.com/joho/godotenv"
+	"github.com/nt2311-vn/invoice-microservices/models"
 )
 
-const baseURL = "https://vinvoice.viettel.vn/api"
+const baseURL = "https://vinvoice.viettel.vn/api/"
 
 type TokenResponse struct {
 	AccessToken    string `json:"access_token"`
@@ -28,37 +29,34 @@ type AuthClient struct {
 	client  *resty.Client
 	token   *TokenResponse
 	tokenAt time.Time
-}
-
-type InvoiceClient struct {
-	client     *resty.Client
-	authClient *AuthClient
+	authObj map[string]string
 }
 
 func NewAuthClient() *AuthClient {
-	client := resty.New()
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("Error loading .env file")
+		return nil
+	}
 
-	return &AuthClient{client: client}
+	client := resty.New()
+	return &AuthClient{
+		client: client,
+		authObj: map[string]string{
+			"username": os.Getenv("INVOICE_CLIENT_USERNAME"),
+			"password": os.Getenv("INVOICE_CLIENT_PASSWORD"),
+		},
+	}
 }
 
 func (c *AuthClient) GetToken() (*TokenResponse, error) {
-	err := godotenv.Load()
-	if err != nil {
-		return nil, err
-	}
-
-	authObj := map[string]string{
-		"username": os.Getenv("INVOICE_CLIENT_USERNAME"),
-		"password": os.Getenv("INVOICE_CLIENT_PASSWORD"),
-	}
-
 	resp, err := c.client.R().
 		SetHeader("Accept", "application/json").
 		SetHeader("Accept-Language", "en-US,en;q=0.9,vi;q=0.8").
 		SetHeader("Content-Type", "application/json;charset=UTF-8").
 		SetHeader("Host", "vinvoice.viettel.vn").
 		SetHeader("Referer", "https://vinvoice.viettel.vn/account/login").
-		SetBody(authObj).Post("https://vinvoice.viettel.vn/api/auth/login")
+		SetBody(c.authObj).Post(baseURL + "auth/login")
 	if err != nil {
 		return nil, err
 	}
@@ -83,14 +81,59 @@ func (c *AuthClient) IsTokenExpired() bool {
 	return time.Since(c.tokenAt).Seconds() >= 250
 }
 
-func NewApiClient() (*InvoiceClient, error) {
-	authClient := NewAuthClient()
-	token, err := authClient.GetToken()
-	if err != nil {
-		return nil, err
+type InvoiceClient struct {
+	client     *resty.Client
+	authClient *AuthClient
+}
+
+func NewInvoiceClient(authClient *AuthClient) (*InvoiceClient, error) {
+	if authClient.IsTokenExpired() {
+		_, err := authClient.GetToken()
+		if err != nil {
+			return nil, fmt.Errorf("error: %v", err)
+		}
 	}
 
-	client := resty.New().SetBaseURL(baseURL).SetAuthToken(token.AccessToken)
-
+	client := resty.New().SetAuthToken(authClient.token.AccessToken)
 	return &InvoiceClient{client: client, authClient: authClient}, nil
+}
+
+func (c *InvoiceClient) FetchInvoices() ([]models.InvoiceResponse, error) {
+	if c.authClient.IsTokenExpired() {
+		token, err := c.authClient.GetToken()
+		if err != nil {
+			return nil, fmt.Errorf("error: %v", err)
+		}
+
+		c.client.SetAuthToken(token.AccessToken)
+	}
+
+	// Set queries with bearer token
+	page := 1
+
+	resp, err := c.client.R().
+		SetQueryParams(map[string]string{
+			"adjustmentType.equals": "1",
+			"supplierId.equals":     "16087",
+			"dataType.equals":       "0",
+			"taxCode.equals":        "0301482205",
+			"sort":                  "issueDate,desc",
+			"invoiceSeri.equals":    "C24MAA",
+			"size":                  "10",
+			"page":                  fmt.Sprintf("%d", page),
+		}).SetHeader("Authorization", fmt.Sprintf("Bearer %s", c.authClient.token.AccessToken)).Get(baseURL + "services/einvoiceapplication/api/invoice/search")
+	if err != nil {
+		return nil, fmt.Errorf("Cannot request invoice: %v\n", err)
+	}
+
+	if resp.IsError() {
+		return nil, fmt.Errorf("error: %v", resp.Error())
+	}
+
+	invResps := []models.InvoiceResponse{}
+
+	err = json.Unmarshal(resp.Body(), &invResps)
+	fmt.Println(invResps)
+
+	return invResps, nil
 }
